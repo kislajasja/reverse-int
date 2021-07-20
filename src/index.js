e0abff36f7eacf6983fac020fc34813006bc9917
Kseniya Halakova <ksenechkasja@mail.ru>
2021-07-20 22:47:08 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = String(n);
+    let result = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        if (str[i] != "-") {
+            result += str[i];
+        }
+    }
+    return Number(result);
+};
